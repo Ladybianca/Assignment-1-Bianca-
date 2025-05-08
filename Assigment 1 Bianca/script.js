ae97e99d39cd6e33ec6fc5208779eb69a9af6223
Ladybianca <bianca.addae@gritacademy.se>
2025-05-08 17:34:01 +0200

    uppg1

---
+++ Assigment 1 Bianca/script.js
@@ -0,0 +1,22 @@
+alert("test")
+console.log("hello world!!!!!")
+
+
+
+let foo = 0
+foo = 10/2
+console.log(foo);
+
+
+
+let bar = 0
+bar = 20/2
+console.log(foo/bar);
+
+
+document.getElementById("back").addEventListener("click", ()=> alert("divide"));
+ 
+
+
+
+
